4e9c8129970b35bbf4e59e08e355ed41572ead00
Sid-stha7 <sidstha444@gmail.com>
2025-11-21 16:25:09 -0500

    feat/music added timer for player and initial login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Player from "./component/Player";
+import Login from "./component/Login";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
           <Player />
+          {/*<Login />*/}
       </header>
     </div>
   );
--- src/component/Player.js
+++ src/component/Player.js
@@ -9,6 +9,46 @@ function Player() {
     const [isPlaying, setIsPlaying] = useState(false);
     const [play, {pause, duration, sound}] = useSound(doiwana);
 
+    const [time, setTime] = useState({
+        min: "00",
+        sec: "00",
+    });
+
+    const [currTime, setCurrTime] = useState({
+        min: "",
+        sec: "",
+    }); // current position of the audio in minutes and seconds
+
+    const [seconds, setSeconds] = useState(); // current position of the audio in seconds
+
+    useEffect(() => {
+        const sec = duration / 1000;
+        const min = Math.floor(sec / 60);
+        const secRemain = Math.floor(sec % 60);
+        const time = {
+            min: min,
+            sec: secRemain
+        };
+        setTime({
+            min: min.toString().padStart(2, '0'), // Pad with '0' for nice display
+            sec: secRemain.toString().padStart(2, '0'),
+        });
+    });
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (sound) {
+                setSeconds(sound.seek([])); // setting the seconds state with the current state
+                const min = Math.floor(sound.seek([]) / 60);
+                const sec = Math.floor(sound.seek([]) % 60);
+                setCurrTime({
+                    min,
+                    sec,
+                });
+            }
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [sound]);
 
     const playingButton = () => {
         if (isPlaying) {
@@ -19,6 +59,7 @@ function Player() {
             setIsPlaying(true);
         }
     };
+
 
 
     return (
@@ -33,6 +74,28 @@ function Player() {
                 <p className="subTitle">Artic Monkey </p>
             </div>
             <div>
+                <div>
+                    <div className="time">
+                        <p>
+                            {currTime.min}:{currTime.sec}
+                        </p>
+                        <p>
+                            {time.min}:{time.sec}
+                        </p>
+                    </div>
+                    <input
+                        type="range"
+                        min="0"
+                        max={duration / 1000}
+                        default="0"
+                        value={seconds}
+                        className="timeline"
+                        onChange={(e) => {
+                            sound.seek([e.target.value]);
+                        }}
+                    />
+                </div>
+                
                 <button className="playButton">
                     <IconContext.Provider value={{size: "3em", color: "#27AE60"}}>
                         <BiSkipPrevious/>
@@ -56,7 +119,11 @@ function Player() {
                         <BiSkipNext/>
                     </IconContext.Provider>
                 </button>
+
+
+
             </div>
+
         </div>
     )
 }
